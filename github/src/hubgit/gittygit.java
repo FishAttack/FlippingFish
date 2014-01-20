7e24a200da32fea9977f95631f634ef3c220fe9b
edunne01 <edunne01@TOOLS16.dcs.bbk.ac.uk>
2014-01-20 20:45:07 +0000

    First Commitment again

---
+++ github/src/hubgit/gittygit.java
@@ -0,0 +1,10 @@
+package hubgit;
+
+public class gittygit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("It's going to be one of those days, isn't it? No?");
+	}
+
+}
